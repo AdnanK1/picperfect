b553dc35eb7de6022214c6b5c912624e686dbe29
AdnanK1 <adnangitonga@gmail.com>
2022-05-28 16:31:15 +0300

    Models: Made models for location,category and image and made migrations

--- base/models.py
+++ base/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='',default='Image')
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    location = models.ForeignKey(Location , on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    updated = models.DateTimeField(auto_now=True) 
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated','-created']
+
+    def __repr__(self):
+        return self.name
+
