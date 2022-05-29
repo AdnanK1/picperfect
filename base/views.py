748e333d03c987afeff9858ecaff9a676cdc019d
AdnanK1 <adnangitonga@gmail.com>
2022-05-29 16:50:17 +0300

    Navbar:Added search functionality to the search bar

--- base/views.py
+++ base/views.py
@@ -1,10 +1,19 @@
 from unicodedata import category
 from django.shortcuts import render
 from .models import Image, Category
+from django.db.models import Q
 
 # Create your views here.
 def home(request):
-    images = Image.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+
+    images = Image.objects.filter(
+        Q(name__icontains=q) |
+        Q(category__name__icontains=q) |
+        Q(description__icontains=q)
+    )
+
     categorys = Category.objects.all()
+
     context = {'images':images,'categorys':categorys}
     return render(request, 'home.html' ,context)
