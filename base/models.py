ab0827080943301f29ff3586a733807aebd364a4
AdnanK1 <adnangitonga@gmail.com>
2022-05-30 14:59:43 +0300

    Tests:added tests to the models so to see if they are working as intended

--- base/models.py
+++ base/models.py
@@ -28,3 +28,24 @@ class Image(models.Model):
     def __str__(self):
         return self.name
 
+    def save_image(self):
+        '''
+        method to save the image
+        '''
+        self.save()
+    
+    @classmethod
+    def search_by_category(cls, search_term):
+        '''
+        method to search image by its category
+        '''
+        image = cls.objects.filter(category__icontains = search_term)
+        return image
+    @classmethod
+    def filter_by_location(cls, search_term):
+        '''
+        method to filter images by their location
+        '''
+        image = cls.objects.filter(location__icontains = search_term)
+        return image 
+
--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from .models import Location, Category, Image
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    '''
+    class that tests of the class model location
+    '''
+    #setup method
+    def setUp(self):
+        self.name = Location(name = '')
+
+    #testing whether we are being instantiated properly
+
+    def test_instance (self):
+        self.assertTrue(isinstance(self.name, Location))
+
+    #testing the save method
+    def test_save_method(self):
+        self.name.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)> 0)
+    
+class ImageTestClass(TestCase):
+    '''
+    test the class model image to ensure the application runs properly
+    '''
+    #setup  method to allow the creation of instances 
+    def setUp(self):
+        self.name = Image(name = '')
+
+    #instance confirming method 
+    def test_instance(self):
+        self.assertTrue(isinstance(self.name, Image))
+
+    #test making sure the image is saved properly 
+    def test_save_method(self):
+        self.name.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
