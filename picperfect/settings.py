1f36e4943ca118374336a2845b1ef38b106ba381
AdnanK1 <adnangitonga@gmail.com>
2022-05-30 17:14:25 +0300

    Deploying to heroku

--- picperfect/settings.py
+++ picperfect/settings.py
@@ -90,9 +90,7 @@ ROOT_URLCONF = 'picperfect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates'
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
